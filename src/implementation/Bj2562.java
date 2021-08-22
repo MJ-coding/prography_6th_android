b30343fc1e76105be9844dcbd346559bb5ea7434
김은조 <zmffheldk@gmail.com>
2021-08-23 00:52:54 +0900

    bj2562

---
+++ src/implementation/Bj2562.java
@@ -0,0 +1,17 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj2562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[9];
+        for(int i =0; i<9; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int mid = 0;
+        int targ
+    }
+}
