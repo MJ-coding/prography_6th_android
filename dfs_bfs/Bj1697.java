1b24a2e5f4030668351d53f3243ef5bfce5127c7
김은조 <zmffheldk@gmail.com>
2022-02-13 17:32:00 +0900

    백준 숨바꼭질 1697 자바 java

--- dfs_bfs/Bj1697.java
+++ dfs_bfs/Bj1697.java
@@ -6,45 +6,54 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
-import java.util.function.BiPredicate;
 
 public class Bj1697 {
     private static int K;
-    private static boolean[] visited;
-    private static final int[] MOVE = {-1,1,2};
+    private static int size;
+    private static int[] visited;
+    private static final int[] MOVE = {2,1,-1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        visited = new boolean[Math.max(N,K) + 1];
+        size = Math.max(N,K)*2;
+        visited = new int[size+10];
 
-        System.out.println(bfs(N));
+        if(K <= N){
+            System.out.println(N-K);
+        }else{
+            System.out.println(bfs(N)-1);
+        }
     }
     private static boolean checkRange(int x){
-        return 0 <= x && x <= K ? true : false;
+        return 0 <= x && x <= K+2 ? true : false;
     }
 
     private static int bfs(int start){
-        int time = 0;
         Queue<Integer> q = new ArrayDeque<>();
-        visited[start] = true;
+        visited[start] = 1;
         q.offer(start);
 
         while(!q.isEmpty()){
             int current = q.poll();
 
-            for(int next : MOVE){
-                if(next == 2) next = current*2;
+            for(int move : MOVE){
+                int next = current;
+                if(move == 2) {
+                    next *= move;
+                } else{
+                    next += move;
+                }
                 if(!checkRange(next)) continue;
-                if(visited[next]) continue;
+                if(visited[next] != 0) continue;
+                if(visited[K] != 0) return visited[K];
 
                 q.offer(next);
-                visited[next] = true;
-                time++;
+                visited[next] = visited[current] + 1;
             }
         }
-        return time;
+        return visited[K];
     }
 }
