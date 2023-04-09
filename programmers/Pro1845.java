566d83728e2622c82eaec9756a9d47a1bbd817fa
김은조 <zmffheldk@gmail.com>
2023-04-09 09:44:03 +0900

    프로그래머스 해시_폰켓몬 Pro1845

---
+++ programmers/Pro1845.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Pro1845 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[] {3,3,3,2,2,4}));
+    }
+
+    public static int solution(int[] nums) {
+        int answer = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int num : nums){
+            map.merge(num,1 , Integer::sum);
+        }
+        
+        int maxPocket = nums.length/2;
+        return maxPocket <= map.size() ? maxPocket : map.size();
+    }
+
+}
