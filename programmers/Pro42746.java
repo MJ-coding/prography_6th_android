e2c891e43c4ab354870b28c2a1ebaccefabcc21e
김은조 <zmffheldk@gmail.com>
2023-04-27 23:53:09 +0900

    가장큰 수

--- programmers/Pro42746.java
+++ programmers/Pro42746.java
@@ -1,29 +1,34 @@
 package programmers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Pro42746 {
 
     public static void main(String[] args) {
-        System.out.println(solution(new int[]{3, 30, 34, 5, 9}) );
+        String[] test = new String[] {"3", "30", "31"};
+        Arrays.sort(test, Comparator.reverseOrder());
+        System.out.println(test[0] + " " + test[1] + " " + test[2]);
+        //System.out.println(solution(new int[]{3, 30, 34, 5, 9}) );
     }
 
     public static String solution(int[] numbers) {
         StringBuilder sb = new StringBuilder();
         int size = numbers.length;
-        List<String> strNumber = new ArrayList<>();
-//1의자리 정렬 2의자리정렬
-        for(int i = 0 ; i < size ; i++){
-            if(numbers[i] < 10){
-                strNumber.add(numbers[i] + "9");
-            }else{
-                strNumber.add(String.valueOf(numbers[i]));
-            }
-        }
-        Collections.reverse(strNumber);
+        String[] strNumber = new String[size];
 
+        for(int i = 0 ; i < size ; i++){
+            strNumber[i] = String.valueOf(numbers[i]);
+        }
+        //Arrays.sort(strNumber,Collections.reverseOrder());
+        //o2+o1 > o1+o2 ? 1 : -1 [1이면 swap]
+        Arrays.sort(strNumber, (o1, o2) -> (o2+o1).compareTo(o1+o2));
+        //0값이 중복일경우 ex){0,0,0}
+        //답이 000이 나오면 안되므로 첫번째값이 0이면 0을 리턴
+        if (strNumber[0].equals("0")) return "0";
         for(String str : strNumber){
             sb.append(str);
         }
